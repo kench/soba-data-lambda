59b7d1e7e8e110832b5a18b9d104fb78fbe29c15
Kenley Cheung <ken@lordkenthegreat.com>
2026-01-26 23:34:52 -0800

    Simplify processing logic

--- app/src/main/java/org/seattleoba/lambda/requesthandler/BevyTicketSQSEventRequestHandler.java
+++ app/src/main/java/org/seattleoba/lambda/requesthandler/BevyTicketSQSEventRequestHandler.java
@@ -51,149 +51,79 @@ public class BevyTicketSQSEventRequestHandler implements RequestHandler<SQSEvent
     public SQSBatchResponse handleRequest(final SQSEvent sqsEvent, final Context context) {
         final List<SQSBatchResponse.BatchItemFailure> batchItemFailures = new ArrayList<>();
 
-        final Map<Integer, String> ticketIdsToMessageIds = new HashMap<>();
-        final Map<Integer, Set<Integer>> eventIdToTicketIds = new HashMap<>();
-        final BiMap<Integer, String> ticketIdsToUserNames = HashBiMap.create();
-
-        sqsEvent.getRecords().forEach(sqsMessage -> {
-            final String messageId = sqsMessage.getMessageId();
+        for (final SQSEvent.SQSMessage message : sqsEvent.getRecords()) {
+            final String messageId = message.getMessageId();
+            final Integer ticketId;
+            final Integer eventId;
+            final String purchaserName;
             try {
                 final BevyTicketEvent bevyTicketEvent =
-                        objectMapper.readValue(sqsMessage.getBody(), BevyTicketEvent.class);
-                final Integer eventId = bevyTicketEvent.eventId();
-                final Integer ticketId = bevyTicketEvent.ticketId();
-                final String userName = bevyTicketEvent.purchaserName().toLowerCase(Locale.ROOT);
-                if (!eventIdToTicketIds.containsKey(eventId)) {
-                    eventIdToTicketIds.put(eventId, new HashSet<>());
-                }
-                eventIdToTicketIds.get(eventId).add(ticketId);
-                ticketIdsToUserNames.put(ticketId, userName);
-                ticketIdsToMessageIds.put(ticketId, messageId);
+                        objectMapper.readValue(message.getBody(), BevyTicketEvent.class);
+                ticketId = bevyTicketEvent.ticketId();
+                eventId = bevyTicketEvent.eventId();
+                purchaserName = bevyTicketEvent.purchaserName();
             } catch (final JsonProcessingException exception) {
                 LOG.error("Unable to parse message {}", messageId, exception);
                 batchItemFailures.add(new SQSBatchResponse.BatchItemFailure(messageId));
+                break;
             }
-        });
 
-        final List<User> users = new ArrayList<>();
-        final List<String> userNames = ticketIdsToUserNames.values().stream().toList();
-        boolean batchRequestFailed = false;
-        try {
-            final UserList userList = twitchClient.getHelix().getUsers(
-                    null,
-                    null,
-                    userNames).execute();
-            users.addAll(userList.getUsers());
-        } catch (final Exception exception) {
-            LOG.error("Unable to retrieve Twitch user ID for user names {}", userNames, exception);
-            batchRequestFailed = true;
-        }
+            final User user;
+            final String userName = purchaserName.toLowerCase();
+            try {
+                final UserList userList = twitchClient.getHelix().getUsers(
+                        null,
+                        null,
+                        Collections.singletonList(userName)).execute();
+                user = userList.getUsers().getFirst();
+            } catch (final Exception exception) {
+                LOG.error("Unable to retrieve Twitch user ID for user name {}", userName, exception);
+                batchItemFailures.add(new SQSBatchResponse.BatchItemFailure(messageId));
+                break;
+            }
 
-        // Batch request failed, fall back to single requests.
-        if (batchRequestFailed) {
-            userNames.forEach(userName -> {
-                try {
-                    final User user = twitchClient.getHelix()
-                            .getUsers(null, null, Collections.singletonList(userName))
-                            .execute().getUsers().getFirst();
-                    users.add(user);
-                } catch (final Exception exception) {
-                    LOG.error("Unable to retrieve Twitch user ID for user name {}", userName, exception);
-                    final Integer ticketId = ticketIdsToUserNames.inverse().get(userName);
-                    final String messageId = ticketIdsToMessageIds.get(ticketId);
-                    batchItemFailures.add(new SQSBatchResponse.BatchItemFailure(messageId));
-                }
-            });
-        }
-
-        final Map<String, TwitchAccount> twitchAccounts = new HashMap<>();
-
-        users.forEach(user -> {
             final TwitchAccount twitchAccount = new TwitchAccount();
-            final String userName = user.getLogin();
             twitchAccount.setId(Integer.parseInt(user.getId()));
-            twitchAccount.setUserName(userName);
+            twitchAccount.setUserName(user.getLogin());
             twitchAccount.setDisplayName(user.getDisplayName());
             twitchAccount.setUserType(user.getType());
             twitchAccount.setBroadcasterType(user.getBroadcasterType());
             twitchAccount.setDescription(user.getDescription());
             twitchAccount.setCreatedAt(user.getCreatedAt().toEpochMilli());
-            twitchAccounts.put(userName, twitchAccount);
-        });
+            final EventRegistration eventRegistration = new EventRegistration();
+            eventRegistration.setEventId(eventId);
+            eventRegistration.setId(ticketId);
+            eventRegistration.setTwitchId(twitchAccount.getId());
 
-        final Set<EventRegistration> eventRegistrations = new HashSet<>();
-
-        for (final Map.Entry<Integer, Set<Integer>> eventEntry : eventIdToTicketIds.entrySet()) {
-            final Integer eventId = eventEntry.getKey();
-            for (final Integer ticketId : eventEntry.getValue()) {
-                final String userName = ticketIdsToUserNames.get(ticketId);
-                if (twitchAccounts.containsKey(userName)) {
-                    final TwitchAccount twitchAccount = twitchAccounts.get(userName);
-                    final EventRegistration eventRegistration = new EventRegistration();
-                    eventRegistration.setEventId(eventId);
-                    eventRegistration.setId(ticketId);
-                    eventRegistration.setTwitchId(twitchAccount.getId());
-                    eventRegistrations.add(eventRegistration);
-                } else {
-                    LOG.error("Unable to find Twitch account ({}) for ticket {}", userName, ticketId);
-                    batchItemFailures.add(new SQSBatchResponse.BatchItemFailure(ticketIdsToMessageIds.get(ticketId)));
+            try {
+                final TwitchAccount item = twitchAccountTable.getItem(twitchAccount);
+                if (Objects.isNull(item)) {
+                    twitchAccountTable.putItem(twitchAccount);
+                } else if (!twitchAccount.equals(item)) {
+                    twitchAccountTable.updateItem(twitchAccount);
                 }
+            } catch (final Exception exception) {
+                LOG.error("Unable to update Twitch accounts", exception);
+                batchItemFailures.add(new SQSBatchResponse.BatchItemFailure(messageId));
+                break;
             }
-        }
 
-        try {
-            persistTwitchAccounts(twitchAccounts.values());
-        } catch (final Exception exception) {
-            LOG.error("Unable to update Twitch accounts", exception);
-            throw new RuntimeException(exception);
-        }
-
-        try {
-            persistEventRegistrations(eventRegistrations);
-        } catch (final Exception exception) {
-            LOG.error("Unable to update event registrations", exception);
-            throw new RuntimeException(exception);
+            try {
+                final EventRegistration item = eventRegistrationTable.getItem(eventRegistration);
+                if (Objects.isNull(item)) {
+                    eventRegistrationTable.putItem(eventRegistration);
+                } else if (!eventRegistration.equals(item)) {
+                    eventRegistrationTable.updateItem(eventRegistration);
+                }
+            } catch (final Exception exception) {
+                LOG.error("Unable to update event registrations", exception);
+                batchItemFailures.add(new SQSBatchResponse.BatchItemFailure(messageId));
+                break;
+            }
         }
 
         return SQSBatchResponse.builder()
                 .withBatchItemFailures(batchItemFailures)
                 .build();
-    }
-
-    private void persistTwitchAccounts(final Collection<TwitchAccount> twitchAccounts) {
-        if (twitchAccounts.isEmpty()) {
-            return;
-        }
-
-        WriteBatch.Builder<TwitchAccount> builder = WriteBatch.builder(TwitchAccount.class);
-        builder = builder.mappedTableResource(twitchAccountTable);
-        for (final TwitchAccount account : twitchAccounts) {
-            builder = builder.addPutItem(account);
-        }
-        final WriteBatch batch = builder.build();
-        final BatchWriteResult result = enhancedClient.batchWriteItem(b -> b.writeBatches(batch));
-        result.unprocessedPutItemsForTable(twitchAccountTable)
-                .forEach(account -> LOG.info(
-                        "Unable to write entry for Twitch account {} ({})",
-                        account.getDisplayName(),
-                        account.getId()));
-    }
-
-    private void persistEventRegistrations(final Collection<EventRegistration> eventRegistrations) {
-        if (eventRegistrations.isEmpty()) {
-            return;
-        }
-
-        WriteBatch.Builder<EventRegistration> builder = WriteBatch.builder(EventRegistration.class);
-        builder = builder.mappedTableResource(eventRegistrationTable);
-        for (final EventRegistration registration : eventRegistrations) {
-            builder = builder.addPutItem(registration);
-        }
-        final WriteBatch batch = builder.build();
-        final BatchWriteResult result = enhancedClient.batchWriteItem(b -> b.writeBatches(batch));
-        result.unprocessedPutItemsForTable(eventRegistrationTable)
-                .forEach(registration -> LOG.error(
-                        "Unable to write event registration for ticket ({})",
-                        registration.getId()));
     }
 }
